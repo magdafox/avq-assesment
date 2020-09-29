aebc5eb564223b153add38612e9a8505e7e0986b
Magdalena Foks <magdalena.foks@onwelo.com>
2020-09-29 08:49:00 +0200

    Add jUnit tests for dice distribution simulation logic

--- src/test/java/com/assesment/avaloq/service/RollResponseMapperTest.java
+++ src/test/java/com/assesment/avaloq/service/RollResponseMapperTest.java
@@ -1,17 +1,71 @@
 package com.assesment.avaloq.service;
 
 import com.assesment.avaloq.domain.Roll;
+import com.assesment.avaloq.domain.RollConfiguration;
+import com.assesment.avaloq.domain.Simulation;
+import com.assesment.avaloq.model.DistributionDetails;
+import com.assesment.avaloq.model.TotalSumDistribution;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @ExtendWith(MockitoExtension.class)
-public class RollResponseMapper {
+class RollResponseMapperTest {
 
     @Test
-    void mapTotalSumDistribution() {
-        List<Roll> rollList;
+    void mapRollsToTotalSumDistributionTest() {
+        Roll roll1 = new Roll();
+        roll1.setTotalSum(1);
+
+        Roll roll2 = new Roll();
+        roll2.setTotalSum(5);
+
+        Roll roll3 = new Roll();
+        roll3.setTotalSum(5);
+
+        List<TotalSumDistribution> totalSums = RollResponseMapper.mapTotalSumDistribution(List.of(roll1, roll2, roll3));
+
+        assertNotNull(totalSums);
+        assertEquals(2, totalSums.size());
+        TotalSumDistribution sum1 = totalSums.get(0);
+        assertNotNull(sum1);
+        assertEquals(1, sum1.getTotalSum());
+        assertEquals(1, sum1.getCount());
+        TotalSumDistribution sum2 = totalSums.get(1);
+        assertNotNull(sum2);
+        assertEquals(5, sum2.getTotalSum());
+        assertEquals(2, sum2.getCount());
+    }
+
+    @Test
+    void mapDistributionList() {
+        Roll roll1 = new Roll();
+        roll1.setTotalSum(1);
+        Roll roll2 = new Roll();
+        roll2.setTotalSum(2);
+
+        Simulation simulation1 = new Simulation();
+        simulation1.setRolls(List.of(roll1, roll2));
+
+        RollConfiguration config1 = new RollConfiguration();
+        config1.setDiceSide(1);
+        config1.setDiceNumber(2);
+        config1.setSimulations(List.of(simulation1));
+
+        List<DistributionDetails> detailsList = RollResponseMapper.mapDistributionList(List.of(config1));
+
+        assertNotNull(detailsList);
+        assertEquals(1, detailsList.size());
+        DistributionDetails details1 = detailsList.get(0);
+        assertNotNull(details1);
+        assertEquals(config1.getDiceNumber(), details1.getDiceNumber());
+        assertEquals(config1.getDiceSide(), details1.getDiceSide());
+        assertEquals(1, details1.getSimulationsNumber());
+        assertEquals(2, details1.getRollsNumber());
     }
 }
--- src/test/java/com/assesment/avaloq/service/RollSimulationServiceTest.java
+++ src/test/java/com/assesment/avaloq/service/RollSimulationServiceTest.java
@@ -1,2 +1,90 @@
-package com.assesment.avaloq.service;public class RollSimulationServiceTest {
+package com.assesment.avaloq.service;
+
+import com.assesment.avaloq.domain.Roll;
+import com.assesment.avaloq.domain.RollConfiguration;
+import com.assesment.avaloq.repository.RollConfigurationRepository;
+import com.assesment.avaloq.repository.SimulationRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class RollSimulationServiceTest {
+
+    @Mock
+    private RollConfigurationRepository rollConfigurationRepository;
+
+    @Mock
+    private SimulationRepository simulationRepository;
+
+    @InjectMocks
+    private RollSimulationService rollSimulationService;
+
+    @ParameterizedTest
+    @MethodSource("numberOfRolls")
+    void executeSimulationSuccess(int numberOfRolls) {
+        RollConfiguration configuration = new RollConfiguration();
+        configuration.setDiceNumber(3);
+        configuration.setDiceSide(6);
+
+        List<Roll> rolls = rollSimulationService.executeSimulation(numberOfRolls, configuration);
+
+        assertNotNull(rolls);
+        assertEquals(numberOfRolls, rolls.size());
+    }
+
+    private static Stream<Arguments> numberOfRolls() {
+        return Stream.of(Arguments.of(0), Arguments.of(10));
+    }
+
+    @Test
+    void getRollConfigurationList_createNewConfiguration() {
+        int diceNumber = 6;
+        int diceSide = 6;
+
+        when(rollConfigurationRepository.save(any(RollConfiguration.class))).then(returnsFirstArg());
+        when(rollConfigurationRepository.findByDiceNumberAndDiceSide(eq(diceNumber), eq(diceSide)))
+                .thenReturn(Optional.empty());
+
+        RollConfiguration rollConfiguration = rollSimulationService.getRollConfiguration(diceNumber, diceSide);
+
+        assertNotNull(rollConfiguration);
+        assertEquals(diceNumber, rollConfiguration.getDiceNumber());
+        assertEquals(diceSide, rollConfiguration.getDiceSide());
+
+        verify(rollConfigurationRepository, times(1)).save(any());
+    }
+
+    @Test
+    void getRollConfigurationList_getOldConfiguration() {
+        int diceNumber = 6;
+        int diceSide = 6;
+
+        when(rollConfigurationRepository.findByDiceNumberAndDiceSide(eq(diceNumber), eq(diceSide)))
+                .thenReturn(Optional.of(new RollConfiguration()));
+
+        RollConfiguration rollConfiguration = rollSimulationService.getRollConfiguration(diceNumber, diceSide);
+
+        assertNotNull(rollConfiguration);
+
+        verify(rollConfigurationRepository, times(0)).save(any());
+    }
 }
